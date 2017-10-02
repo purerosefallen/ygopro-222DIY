146b2f2790ef22dc56ab54ff2ac6a0fd48187eea
篁楠 <HuangYuNan@users.noreply.github.com>
2017-10-03 00:38:37 +0800

    Update c12001002.lua

--- expansions/script/c12001002.lua
+++ expansions/script/c12001002.lua
@@ -44,7 +44,7 @@ function c12001002.operation(e,tp,eg,ep,ev,re,r,rp)
 		Duel.DisableShuffleCheck()
 		Duel.SendtoGrave(g,REASON_EFFECT+REASON_REVEAL)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g=Duel.SelectMatchingCard(tp,c12000002.filter,tp,LOCATION_DECK,0,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,c12001002.filter,tp,LOCATION_DECK,0,1,1,nil)
 		  if g:GetCount()>0 then
 			Duel.SendtoHand(g,nil,REASON_EFFECT)
 			Duel.ConfirmCards(1-tp,g)
