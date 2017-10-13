0bf52fc6286617adbd7bd9d621bcfb908fa69c3a
purerosefallen <78877@qq.com>
2017-10-14 00:35:20 +0800

    adj

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -1482,8 +1482,8 @@ void Game::ShowCardInfo(int code) {
 		stDataInfo->setText(formatBuffer);
 		//modded
 		if ((cd.type & TYPE_LINK) && (cd.level == 8)) {
-			stDataInfo->setRelativePosition(rect<s32>(30, 60, 296, 83));
-			stSetName->setRelativePosition(rect<s32>(15, 98, 296, 106));
+			stDataInfo->setRelativePosition(rect<s32>(15, 60, 296, 98));
+			stSetName->setRelativePosition(rect<s32>(15, 98, 296, 121));
 			stText->setRelativePosition(rect<s32>(15, 98 + offset, 287, 324));
 			scrCardText->setRelativePosition(rect<s32>(267, 98 + offset, 287, 324));
 		} else {
