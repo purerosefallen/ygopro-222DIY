ff4b39fff6b08f3c9515a66733c26d7e95001110
purerosefallen <78877@qq.com>
2017-11-25 15:12:32 +0800

    fixoslib

--- gframe/config.h
+++ gframe/config.h
@@ -51,8 +51,13 @@ inline int _wtoi(const wchar_t * s) {
 #ifdef _WIN32
 #include <irrKlang.h>
 #endif
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#else
 #include <GL/gl.h>
 #include <GL/glu.h>
+#endif
 #include "CGUITTFont.h"
 #include "CGUIImageButton.h"
 #include <iostream>
