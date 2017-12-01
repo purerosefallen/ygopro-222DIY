ebf8f8d459cb4dc24a7da0bf32480b5772d6c75d
purerosefallen <78877@qq.com>
2017-12-01 12:59:52 +0800

    format

--- premake4.lua
+++ premake4.lua
@@ -1,63 +1,63 @@
 solution "ygo"
-    location "build"
-    language "C++"
-    objdir "obj"
+	location "build"
+	language "C++"
+	objdir "obj"
 	startproject "ygopro"
 
-    configurations { "Debug", "Release" }
+	configurations { "Debug", "Release" }
 	defines { "LUA_COMPAT_5_2" }
 
-    configuration "windows"
-        defines { "WIN32", "_WIN32" }
+	configuration "windows"
+		defines { "WIN32", "_WIN32" }
 
-    configuration "bsd"
-        defines { "LUA_USE_POSIX" }
-        includedirs { "/usr/local/include" }
-        libdirs { "/usr/local/lib" }
+	configuration "bsd"
+		defines { "LUA_USE_POSIX" }
+		includedirs { "/usr/local/include" }
+		libdirs { "/usr/local/lib" }
 
-    configuration "macosx"
-        defines { "LUA_USE_MACOSX" }
-        includedirs { "/usr/local/include/*" }
-        libdirs { "/usr/local/lib", "/usr/X11/lib" }
-        buildoptions { "-stdlib=libc++" }
-        links {"OpenGL.framework","Cocoa.framework","IOKit.framework"}
+	configuration "macosx"
+		defines { "LUA_USE_MACOSX" }
+		includedirs { "/usr/local/include/*" }
+		libdirs { "/usr/local/lib", "/usr/X11/lib" }
+		buildoptions { "-stdlib=libc++" }
+		links {"OpenGL.framework","Cocoa.framework","IOKit.framework"}
 
-    configuration "linux"
-        defines { "LUA_USE_LINUX" }
+	configuration "linux"
+		defines { "LUA_USE_LINUX" }
 
-    configuration "vs*"
-        flags "EnableSSE2"
-        buildoptions { "-wd4996" }
-        defines { "_CRT_SECURE_NO_WARNINGS" }
+	configuration "vs*"
+		flags "EnableSSE2"
+		buildoptions { "-wd4996" }
+		defines { "_CRT_SECURE_NO_WARNINGS" }
 
-    configuration "not vs*"
-        buildoptions { "-fno-strict-aliasing", "-Wno-multichar" }
-    configuration {"not vs*", "windows"}
-        buildoptions { "-static-libgcc" }
+	configuration "not vs*"
+		buildoptions { "-fno-strict-aliasing", "-Wno-multichar" }
+	configuration {"not vs*", "windows"}
+		buildoptions { "-static-libgcc" }
 
-    configuration "Debug"
-        flags "Symbols"
-        defines "_DEBUG"
-        targetdir "bin/debug"
+	configuration "Debug"
+		flags "Symbols"
+		defines "_DEBUG"
+		targetdir "bin/debug"
 
-    configuration { "Release", "not vs*" }
-        flags "Symbols"
-        defines "NDEBUG"
-        buildoptions "-march=native"
+	configuration { "Release", "not vs*" }
+		flags "Symbols"
+		defines "NDEBUG"
+		buildoptions "-march=native"
 
-    configuration { "Debug", "vs*" }
-        defines { "_ITERATOR_DEBUG_LEVEL=0" }
+	configuration { "Debug", "vs*" }
+		defines { "_ITERATOR_DEBUG_LEVEL=0" }
 
-    configuration "Release"
-        --flags { "OptimizeSpeed" }
-        targetdir "bin/release"
+	configuration "Release"
+		--flags { "OptimizeSpeed" }
+		targetdir "bin/release"
 
-    include "ocgcore"
-    include "gframe"
-    if os.is("windows") then
-    include "event"
-    include "freetype"
-    include "irrlicht"
-    include "lua"
-    include "sqlite3"
-    end
+	include "ocgcore"
+	include "gframe"
+	if os.is("windows") then
+	include "event"
+	include "freetype"
+	include "irrlicht"
+	include "lua"
+	include "sqlite3"
+	end
