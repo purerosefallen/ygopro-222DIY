4aa2bd34a1a1ee64f1943bf45cd881c277dbfc9f
purerosefallen <78877@qq.com>
2017-12-01 13:14:28 +0800

    irrtest

--- premake5.lua
+++ premake5.lua
@@ -1,81 +1,81 @@
 solution "ygo"
-	location "build"
-	language "C++"
-	objdir "obj"
-	if os.ishost("windows") or os.getenv("USE_IRRKLANG") then
-		USE_IRRKLANG = true
-		if os.getenv("irrklang_pro") then
-			IRRKLANG_PRO = true
-		end
-	end
-	startproject "ygopro"
+    location "build"
+    language "C++"
+    objdir "obj"
+    if os.ishost("windows") or os.getenv("USE_IRRKLANG") then
+        USE_IRRKLANG = true
+        if os.getenv("irrklang_pro") then
+            IRRKLANG_PRO = true
+        end
+    end
+    startproject "ygopro"
 
-	configurations { "Debug", "Release" }
-	defines { "LUA_COMPAT_5_2" }
+    configurations { "Debug", "Release" }
+    defines { "LUA_COMPAT_5_2" }
 
-	configuration "windows"
-		defines { "WIN32", "_WIN32", "WINVER=0x0501" }
-		libdirs { "$(DXSDK_DIR)Lib/x86" }
-		entrypoint "mainCRTStartup"
-		toolset "v140_xp"
-		startproject "ygopro"
+    configuration "windows"
+        defines { "WIN32", "_WIN32", "WINVER=0x0501" }
+        libdirs { "$(DXSDK_DIR)Lib/x86" }
+        entrypoint "mainCRTStartup"
+        toolset "v140_xp"
+        startproject "ygopro"
 
-	configuration "bsd"
-		defines { "LUA_USE_POSIX" }
-		includedirs { "/usr/local/include" }
-		libdirs { "/usr/local/lib" }
+    configuration "bsd"
+        defines { "LUA_USE_POSIX" }
+        includedirs { "/usr/local/include" }
+        libdirs { "/usr/local/lib" }
 
-	configuration "macosx"
-		defines { "LUA_USE_MACOSX" }
-		includedirs { "/usr/local/include", "/usr/local/include/*" }
-		libdirs { "/usr/local/lib", "/usr/X11/lib" }
-		buildoptions { "-stdlib=libc++" }
-		links { "OpenGL.framework", "Cocoa.framework", "IOKit.framework" }
+    configuration "macosx"
+        defines { "LUA_USE_MACOSX" }
+        includedirs { "/usr/local/include", "/usr/local/include/*" }
+        libdirs { "/usr/local/lib", "/usr/X11/lib" }
+        buildoptions { "-stdlib=libc++" }
+        links { "OpenGL.framework", "Cocoa.framework", "IOKit.framework" }
 
-	configuration "linux"
-		defines { "LUA_USE_LINUX" }
+    configuration "linux"
+        defines { "LUA_USE_LINUX" }
 
-	configuration "Release"
-		optimize "Speed"
-		targetdir "bin/release"
+    configuration "Release"
+        optimize "Speed"
+        targetdir "bin/release"
 
-	configuration "Debug"
-		symbols "On"
-		defines "_DEBUG"
-		targetdir "bin/debug"
+    configuration "Debug"
+        symbols "On"
+        defines "_DEBUG"
+        targetdir "bin/debug"
 
-	configuration { "Release", "vs*" }
-		flags { "StaticRuntime", "LinkTimeOptimization" }
-		disablewarnings { "4244", "4267", "4838", "4577", "4819", "4018", "4996", "4477" }
+    configuration { "Release", "vs*" }
+        flags { "StaticRuntime", "LinkTimeOptimization" }
+        disablewarnings { "4244", "4267", "4838", "4577", "4819", "4018", "4996", "4477" }
 
-	configuration { "Release", "not vs*" }
-		symbols "On"
-		defines "NDEBUG"
-		buildoptions "-march=native"
+    configuration { "Release", "not vs*" }
+        symbols "On"
+        defines "NDEBUG"
+        buildoptions "-march=native"
 
-	configuration { "Debug", "vs*" }
-		defines { "_ITERATOR_DEBUG_LEVEL=0" }
-		disablewarnings { "4819" }
+    configuration { "Debug", "vs*" }
+        defines { "_ITERATOR_DEBUG_LEVEL=0" }
+        disablewarnings { "4819" }
 
-	configuration "vs*"
-		vectorextensions "SSE2"
-		defines { "_CRT_SECURE_NO_WARNINGS" }
-	
-	configuration "not vs*"
-		buildoptions { "-fno-strict-aliasing", "-Wno-multichar" }
+    configuration "vs*"
+        vectorextensions "SSE2"
+        defines { "_CRT_SECURE_NO_WARNINGS" }
+    
+    configuration "not vs*"
+        buildoptions { "-fno-strict-aliasing", "-Wno-multichar" }
 
-	configuration {"not vs*", "windows"}
-		buildoptions { "-static-libgcc" }
+    configuration {"not vs*", "windows"}
+        buildoptions { "-static-libgcc" }
 
-	include "ocgcore"
-	include "gframe"
-	if os.ishost("windows") then
-		include "event"
-		include "freetype"
-		include "irrlicht"
-		include "lua"
-		include "sqlite3"
-		if IRRKLANG_PRO then
-			include "ikpmp3"
-		end
-	end
+    include "ocgcore"
+    include "gframe"
+    if os.ishost("windows") then
+        include "event"
+        include "freetype"
+        include "irrlicht"
+        include "lua"
+        include "sqlite3"
+        if IRRKLANG_PRO then
+            include "ikpmp3"
+        end
+    end
