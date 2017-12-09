edcbab7448d2ed207818f4c5fc7d0723016e073b
purerosefallen <78877@qq.com>
2017-12-09 09:19:44 +0800

    startup

--- start.bat
+++ start.bat
@@ -1,7 +1,14 @@
 @echo off
+
 cd ygopro-server/redis
+
 start redis-server.exe
+
 cd ../windbot
-start windbot.exe 2399
+
+start windbot.exe servermode=true serverport=2399
 cd ..
+
 node.exe ygopro-server.js
+
+pause
