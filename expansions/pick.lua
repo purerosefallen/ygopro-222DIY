137219c29c60be53851678a37b07472f80bf5ac4
Nanahira <78877@qq.com>
2017-12-10 19:22:26 +0800

    Pick facedown

--- expansions/pick.lua
+++ expansions/pick.lua
@@ -66,10 +66,12 @@ function Auxiliary.SinglePick(p,list,count)
 			local code=list[math.random(#list)]
 			g:AddCard(Duel.CreateToken(p,code))
 		end
-		Duel.Remove(g,POS_FACEUP,REASON_RULE)
+		Duel.Remove(g,POS_FACEDOWN,REASON_RULE)
 	end
 	local sg=g1:Clone()
 	sg:Merge(g2)
+	Duel.ConfirmCards(p,sg)
+	Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
 	local sc=sg:Select(p,1,1,nil):GetFirst()
 	local tg=g1:IsContains(sc) and g1 or g2
 	local rg=g1:IsContains(sc) and g2 or g1
