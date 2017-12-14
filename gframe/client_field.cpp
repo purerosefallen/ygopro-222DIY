95217ec7f677f00978e30f834b8ac8216c878a6d
mercury233 <me@mercury233.me>
2017-12-15 01:01:56 +0800

    merge_sound

--- gframe/client_field.cpp
+++ gframe/client_field.cpp
@@ -1038,7 +1038,6 @@ bool ClientField::ShowSelectSum(bool panelmode) {
 				BufferIO::CopyWStrRef(dataManager.GetSysString(210), pwbuf, 256);
 				mainGame->stQMessage->setText(wbuf);
 				mainGame->PopupElement(mainGame->wQuery);
-				mainGame->PlaySoundEffect(SOUND_QUESTION);
 			}
 		} else {
 			select_ready = false;
@@ -1060,7 +1059,6 @@ bool ClientField::ShowSelectSum(bool panelmode) {
 				BufferIO::CopyWStrRef(dataManager.GetSysString(210), pwbuf, 256);
 				mainGame->stQMessage->setText(wbuf);
 				mainGame->PopupElement(mainGame->wQuery);
-				mainGame->PlaySoundEffect(SOUND_QUESTION);
 			}
 		} else
 			select_ready = false;
