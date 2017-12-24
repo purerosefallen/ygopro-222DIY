f156fc6207cc7fa26dd03e553476c0840773eafb
Smile-DK <2236141431@qq.com>
2017-12-24 13:30:15 +0000

    Push by Appveyor

---
+++ expansions/script/c13257230.lua
@@ -0,0 +1,138 @@
+--B.E.S 巨核Mk-2改
+function c13257230.initial_effect(c)
+	c:EnableCounterPermit(0x1f)
+	local e11=Effect.CreateEffect(c)
+	e11:SetDescription(aux.Stringid(13257230,1))
+	e11:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e11:SetType(EFFECT_TYPE_SINGLE)
+	e11:SetCode(EFFECT_SUMMON_PROC)
+	e11:SetCondition(c13257230.otcon)
+	e11:SetOperation(c13257230.otop)
+	e11:SetValue(SUMMON_TYPE_ADVANCE)
+	c:RegisterEffect(e11)
+	--Destroy replace
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DESTROY_REPLACE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(c13257230.desreptg)
+	e1:SetOperation(c13257230.desrepop)
+	c:RegisterEffect(e1)
+	--counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetOperation(c13257230.ctop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(13257330,3))
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_BATTLE_DESTROYING)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(aux.bdogcon)
+	e4:SetTarget(c13257230.catg)
+	e4:SetOperation(c13257230.caop)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(13257330,3))
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCategory(CATEGORY_DESTROY)
+	e5:SetCode(EVENT_BATTLE_DAMAGE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCondition(c13257230.descon)
+	e5:SetTarget(c13257230.destg)
+	e5:SetOperation(c13257230.desop)
+	c:RegisterEffect(e5)
+	local e12=Effect.CreateEffect(c)
+	e12:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e12:SetCode(EVENT_SUMMON_SUCCESS)
+	e12:SetOperation(c13257230.bgmop)
+	c:RegisterEffect(e12)
+	local e13=e12:Clone()
+	e13:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e13)
+	
+end
+function c13257230.otfilter(c)
+	return c:IsSetCard(0x353) and c:IsType(TYPE_MONSTER) and c:IsReleasable()
+end
+function c13257230.otfilter1(c)
+	return c:IsSetCard(0x353) and c:IsType(TYPE_MONSTER) and c:IsFaceup()
+end
+function c13257230.otcon(e,c,minc)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local mg=Duel.GetMatchingGroup(c13257230.otfilter,tp,LOCATION_HAND,0,c)
+	local mg1=Duel.GetMatchingGroup(c13257230.otfilter1,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	return c:GetLevel()>6 and minc<=2
+		and (Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and mg:GetCount()>=1
+			or Duel.CheckTribute(c,1,1,mg1))
+		or c:GetLevel()>4 and c:GetLevel()<=6 and minc<=1
+			and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and mg:GetCount()>=1
+end
+function c13257230.otop(e,tp,eg,ep,ev,re,r,rp,c)
+	local mg=Duel.GetMatchingGroup(c13257230.otfilter,tp,LOCATION_HAND,0,c)
+	local mg1=Duel.GetMatchingGroup(c13257230.otfilter1,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	local sg=Group.CreateGroup()
+	if c:GetLevel()>6 then
+		if mg:GetCount()>0 and mg1:GetCount()==0 or (mg:GetCount()>0 and mg1:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(13257230,2))) then
+			sg=mg:Select(tp,1,1,nil)
+		else
+			sg=Duel.SelectTribute(tp,c,1,1,mg1)
+		end
+	else
+		sg=mg:Select(tp,1,1,nil)
+	end
+	c:SetMaterial(sg)
+	Duel.Release(sg,REASON_SUMMON+REASON_MATERIAL)
+end
+function c13257230.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReason(REASON_EFFECT+REASON_BATTLE)
+		and e:GetHandler():GetCounter(0x1f)>0 end
+	return true
+end
+function c13257230.desrepop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RemoveCounter(ep,0x1f,1,REASON_EFFECT)
+end
+function c13257230.ctop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():AddCounter(0x1f,2)
+end
+function c13257230.catg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsChainAttackable(0,true) end
+end
+function c13257230.caop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToBattle() or not c:IsRelateToEffect(e) then return end
+	Duel.ChainAttack()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_BATTLE+PHASE_DAMAGE_CAL)
+	c:RegisterEffect(e1)
+end
+function c13257230.descon(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp and eg:GetFirst()==e:GetHandler()
+end
+function c13257230.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,0,0)
+end
+function c13257230.desop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.HintSelection(g)
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
+function c13257230.bgmop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(11,0,aux.Stringid(13257230,4))
+end
---
+++ expansions/script/c13257231.lua
@@ -0,0 +1,97 @@
+--B.E.S 水核
+function c13257231.initial_effect(c)
+	--pos
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SET_POSITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,LOCATION_MZONE)
+	e1:SetCondition(c13257231.poscon)
+	e1:SetTarget(c13257231.postg)
+	e1:SetValue(POS_FACEUP_DEFENSE)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(13257231,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCountLimit(1)
+	e2:SetTarget(c13257231.target)
+	e2:SetOperation(c13257231.operation)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(13257231,1))
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCondition(c13257231.tgcon)
+	e3:SetTarget(c13257231.tgtg)
+	e3:SetOperation(c13257231.tgop)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EVENT_REMOVE)
+	c:RegisterEffect(e4)
+	--indes
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetValue(c13257231.indct)
+	c:RegisterEffect(e5)
+	local e12=Effect.CreateEffect(c)
+	e12:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e12:SetCode(EVENT_SUMMON_SUCCESS)
+	e12:SetOperation(c13257231.bgmop)
+	c:RegisterEffect(e12)
+	local e13=e12:Clone()
+	e13:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e13)
+	
+end
+function c13257231.poscon(e)
+	return e:GetHandler():IsDefensePos()
+end
+function c13257231.postg(e,c)
+	return c:IsFaceup()
+end
+function c13257231.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsDefensePos() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsDefensePos,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,Card.IsDefensePos,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,HINTMSG_DESTROY,g,1,0,0)
+end
+function c13257231.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
+function c13257231.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return rp~=tp and c:IsReason(REASON_EFFECT)
+end
+function c13257231.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDefensePos(),tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,0,LOCATION_MZONE)
+end
+function c13257231.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsDefensePos,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	if g:GetCount()>0 then
+		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_DESTROY)
+		local sg=g:Select(1-tp,1,1,nil)
+		Duel.HintSelection(sg)
+		Duel.Destroy(sg,REASON_RULE)
+	end
+end
+function c13257231.bgmop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(11,0,aux.Stringid(13257231,4))
+end
+function c13257231.indct(e,re,r,rp)
+	if bit.band(r,REASON_BATTLE+REASON_EFFECT)~=0 then
+		return 1
+	else return 0 end
+end
