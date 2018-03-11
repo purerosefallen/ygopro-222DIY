73e00ccd1e5801c2546d6c65994f175760e3fee8
purerosefallen <78877@qq.com>
2018-03-11 17:56:21 +0800

    fix

--- gframe/drawing.cpp
+++ gframe/drawing.cpp
@@ -461,16 +461,16 @@ void Game::DrawMisc() {
 	}
 	driver->draw2DImage(imageManager.tLPFrame, recti(330, 10, 629, 30), recti(0, 0, 200, 20), 0, 0, true);
 	driver->draw2DImage(imageManager.tLPFrame, recti(691, 10, 990, 30), recti(0, 0, 200, 20), 0, 0, true);
-	if(!dinfo.start_lp[0])
+	if(!dInfo.start_lp[0])
 		driver->draw2DImage(imageManager.tLPBar, recti(335, 12, 335, 28), recti(0, 0, 16, 16), 0, 0, true);
-	else if(dInfo.lp[0] >= dinfo.start_lp[0])
+	else if(dInfo.lp[0] >= dInfo.start_lp[0])
 		driver->draw2DImage(imageManager.tLPBar, recti(335, 12, 625, 28), recti(0, 0, 16, 16), 0, 0, true);
-	else driver->draw2DImage(imageManager.tLPBar, recti(335, 12, 335 + 290 * dInfo.lp[0] / dinfo.start_lp[0], 28), recti(0, 0, 16, 16), 0, 0, true);
-	if(!dinfo.start_lp[1])
+	else driver->draw2DImage(imageManager.tLPBar, recti(335, 12, 335 + 290 * dInfo.lp[0] / dInfo.start_lp[0], 28), recti(0, 0, 16, 16), 0, 0, true);
+	if(!dInfo.start_lp[1])
 		driver->draw2DImage(imageManager.tLPBar, recti(986, 12, 986, 28), recti(0, 0, 16, 16), 0, 0, true);
-	else if(dInfo.lp[1] >= dinfo.start_lp[1])
+	else if(dInfo.lp[1] >= dInfo.start_lp[1])
 		driver->draw2DImage(imageManager.tLPBar, recti(696, 12, 986, 28), recti(0, 0, 16, 16), 0, 0, true);
-	else driver->draw2DImage(imageManager.tLPBar, recti(986 - 290 * dInfo.lp[1] / dinfo.start_lp[1], 12, 986, 28), recti(0, 0, 16, 16), 0, 0, true);
+	else driver->draw2DImage(imageManager.tLPBar, recti(986 - 290 * dInfo.lp[1] / dInfo.start_lp[1], 12, 986, 28), recti(0, 0, 16, 16), 0, 0, true);
 	if(lpframe) {
 		dInfo.lp[lpplayer] -= lpd;
 		myswprintf(dInfo.strLP[lpplayer], L"%d", dInfo.lp[lpplayer]);
